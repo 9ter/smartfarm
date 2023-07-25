9156bb07f074215295fbc28cb0b5986c8420ae11
ALEXANDERSSONN <payupater@gmail.com>
2023-07-25 18:44:08 +0700

    1

--- pages/login.php
+++ pages/login.php
@@ -19,8 +19,6 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         // ล็อกอินไม่สำเร็จ
         echo "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง!";
     }
-}else {
-    header("Location: login.html");
 }
 
 ?>
