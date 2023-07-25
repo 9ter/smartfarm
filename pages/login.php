449a4c7d5ee0968abddf6542c6646e2be8dd081c
ALEXANDERSSONN <payupater@gmail.com>
2023-07-25 18:36:36 +0700

    1

--- pages/login.php
+++ pages/login.php
@@ -2,11 +2,24 @@
 
 include '../config.php';
 
-$_POST['username'];
+$myusername = $_POST['username'];
+
+$mypassword = $_POST['pass'];
 
 
 if(!isset($_POST['username'])  && !isset($_POST['pass'])){
     header("Location: login.html");
+}
+
+$sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // ล็อกอินสำเร็จ
+    echo "ล็อกอินสำเร็จ!";
+} else {
+    // ล็อกอินไม่สำเร็จ
+    echo "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง!";
 }
 
 ?>
