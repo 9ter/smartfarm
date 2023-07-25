fa60fcf440af80bc91fd290de6668be727fbacab
ALEXANDERSSONN <payupater@gmail.com>
2023-07-25 18:45:43 +0700

    1

--- pages/login.php
+++ pages/login.php
@@ -1,6 +1,17 @@
 <?php
 
-include '../config.php';
+$servername = "localhost";
+$username = "thanankornm";
+$password = "BrqHAmTC3w_ZRCut";
+$dbname = "smartfarm";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
     // รับข้อมูลจากฟอร์มล็อกอิน
