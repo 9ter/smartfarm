38ce1292a0e98933c56a6bfc86126b4ecc360d70
ALEXANDERSSONN <payupater@gmail.com>
2023-07-25 19:10:44 +0700

    1

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $sql = "SELECT * FROM sensor_box_1 ";
 $result = $conn->query($sql);
 
 while ($row = mysqli_fetch_array($result)) {
-  echo $row[1], $row[2];  
+  
 }
 
 ?>
@@ -37,15 +37,6 @@ while ($row = mysqli_fetch_array($result)) {
 <html lang="en">
 
 <head>
-
-
-
-
-
-
-
-
-
 
 
 
@@ -371,85 +362,15 @@ while ($row = mysqli_fetch_array($result)) {
                   <div class="d-flex flex-column h-100">
                     <h2 class="font-weight-bolder mb-0">General Statistics</h2>
                   </div>
-
                 </div>
               </div>
             </div>
           </div>
 
           <div class="row">
-            <div class="col-lg-5 col-sm-5">
-              <div class="card  mb-2">
-                <div class="card-header p-3 pt-2">
-                  <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-xl mt-n4 position-absolute">
-                    <i class="material-icons opacity-10">weekend</i>
-                  </div>
-                  <div class="text-end pt-1">
-                    <p class="text-sm mb-0 text-capitalize">Bookings</p>
-                    <h4 class="mb-0">281</h4>
-                  </div>
-                </div>
 
-                <hr class="dark horizontal my-0">
-                <div class="card-footer p-3">
-                  <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than last week</p>
-                </div>
-              </div>
-
-              <div class="card  mb-2">
-                <div class="card-header p-3 pt-2">
-                  <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary shadow text-center border-radius-xl mt-n4 position-absolute">
-                    <i class="material-icons opacity-10">leaderboard</i>
-                  </div>
-                  <div class="text-end pt-1">
-                    <p class="text-sm mb-0 text-capitalize">Today's Users</p>
-                    <h4 class="mb-0">2,300</h4>
-                  </div>
-                </div>
-
-                <hr class="dark horizontal my-0">
-                <div class="card-footer p-3">
-                  <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+3% </span>than last month</p>
-                </div>
-              </div>
-
-            </div>
-            <div class="col-lg-5 col-sm-5 mt-sm-0 mt-4">
-              <div class="card  mb-2">
-                <div class="card-header p-3 pt-2 bg-transparent">
-                  <div class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
-                    <i class="material-icons opacity-10">store</i>
-                  </div>
-                  <div class="text-end pt-1">
-                    <p class="text-sm mb-0 text-capitalize ">Revenue</p>
-                    <h4 class="mb-0 ">34k</h4>
-                  </div>
-                </div>
-
-                <hr class="horizontal my-0 dark">
-                <div class="card-footer p-3">
-                  <p class="mb-0 "><span class="text-success text-sm font-weight-bolder">+1% </span>than yesterday</p>
-                </div>
-              </div>
-
-              <div class="card ">
-                <div class="card-header p-3 pt-2 bg-transparent">
-                  <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
-                    <i class="material-icons opacity-10">person_add</i>
-                  </div>
-                  <div class="text-end pt-1">
-                    <p class="text-sm mb-0 text-capitalize ">Followers</p>
-                    <h4 class="mb-0 ">+91</h4>
-                  </div>
-                </div>
-
-                <hr class="horizontal my-0 dark">
-                <div class="card-footer p-3">
-                  <p class="mb-0 ">Just updated</p>
-                </div>
-              </div>
-
-            </div>
+          <iframe src="http://lesyslab.com:1884/ui/#!/0?socketid=QTBxpqlSHbQN8mqkAAER" title="description"></iframe>
+         
           </div>
 
           <div class="row mt-4">
