895a667fc9ce6ad8086a8e1e2c51e4b7d9aec481
9ter <kasiyapo@gmail.com>
2023-07-26 17:05:44 +0700

    1

--- index.php
+++ index.php
@@ -87,25 +87,35 @@ while ($row = mysqli_fetch_array($result)) {
 
 
 <body class="g-sidenav-show  bg-gray-100">
+
+
+
+
+
   <aside
-    class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark"
+    class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark"
     id="sidenav-main">
+
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
         aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard "
-        target="_blank">
-        <img src="./assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo" />
-        <span class="ms-1 font-weight-bold text-white">Material Dashboard 2</span>
+      <a class="navbar-brand m-0" href="" target="_blank">
+        <img src="./assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
+        <span class="ms-1 font-weight-bold text-white">Smartfarm Dashboard </span>
       </a>
     </div>
 
-    <hr class="horizontal light mt-0 mb-2" />
 
-    <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
+    <hr class="horizontal light mt-0 mb-2">
+
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
       <ul class="navbar-nav">
+
+
+
         <li class="nav-item">
-          <a class="nav-link text-white" href="./dashboard.html">
+          <a class="nav-link text-white " href="./dashboard.html">
+
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">dashboard</i>
             </div>
@@ -113,9 +123,9 @@ while ($row = mysqli_fetch_array($result)) {
             <span class="nav-link-text ms-1">Dashboard</span>
           </a>
         </li>
-
         <li class="nav-item">
-          <a class="nav-link text-white" href="./tables.html">
+          <a class="nav-link text-white " href="./tables.html">
+
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">table_view</i>
             </div>
@@ -123,29 +133,9 @@ while ($row = mysqli_fetch_array($result)) {
             <span class="nav-link-text ms-1">Tables</span>
           </a>
         </li>
-
         <li class="nav-item">
-          <a class="nav-link text-white" href="./billing.html">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">receipt_long</i>
-            </div>
+          <a class="nav-link text-white " href="./rtl.html">
 
-            <span class="nav-link-text ms-1">Billing</span>
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link text-white" href="./virtual-reality.html">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">view_in_ar</i>
-            </div>
-
-            <span class="nav-link-text ms-1">Virtual Reality</span>
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link text-white" href="./rtl.html">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">format_textdirection_r_to_l</i>
             </div>
@@ -153,9 +143,9 @@ while ($row = mysqli_fetch_array($result)) {
             <span class="nav-link-text ms-1">RTL</span>
           </a>
         </li>
-
         <li class="nav-item">
-          <a class="nav-link text-white" href="./notifications.html">
+          <a class="nav-link text-white " href="./notifications.html">
+
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">notifications</i>
             </div>
@@ -163,15 +153,12 @@ while ($row = mysqli_fetch_array($result)) {
             <span class="nav-link-text ms-1">Notifications</span>
           </a>
         </li>
-
         <li class="nav-item mt-3">
-          <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">
-            Account pages
-          </h6>
+          <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Account pages</h6>
         </li>
-
         <li class="nav-item">
-          <a class="nav-link text-white" href="./profile.html">
+          <a class="nav-link text-white " href="./pages/profile.php">
+
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">person</i>
             </div>
@@ -179,19 +166,20 @@ while ($row = mysqli_fetch_array($result)) {
             <span class="nav-link-text ms-1">Profile</span>
           </a>
         </li>
-
         <li class="nav-item">
-          <a class="nav-link text-white" href="./sign-in.html">
+          <a class="nav-link text-white " href="./sign-in.html">
+
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">login</i>
             </div>
-
             <span class="nav-link-text ms-1">Sign In</span>
           </a>
         </li>
 
+
         <li class="nav-item">
-          <a class="nav-link text-white" href="./sign-up.html">
+          <a class="nav-link text-white " href="./pages/sign-up.php">
+
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">assignment</i>
             </div>
@@ -202,52 +190,26 @@ while ($row = mysqli_fetch_array($result)) {
       </ul>
     </div>
 
-    <div class="sidenav-footer position-absolute w-100 bottom-0">
-      <div class="mx-3">
-        <a class="btn btn-outline-primary mt-4 w-100"
-          href="https://www.creative-tim.com/learning-lab/bootstrap/overview/material-dashboard?ref=sidebarfree"
-          type="button">Documentation</a>
-        <a class="btn bg-gradient-primary w-100"
-          href="https://www.creative-tim.com/product/material-dashboard-pro?ref=sidebarfree" type="button">Upgrade to
-          pro</a>
-      </div>
+    <div class="sidenav-footer position-absolute w-100 bottom-0 ">
     </div>
+
   </aside>
 
-  <main class="main-content border-radius-lg">
+  <main class="main-content border-radius-lg ">
     <!-- Navbar -->
 
     <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
       data-scroll="true">
       <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
-          <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-            <li class="breadcrumb-item text-sm">
-              <a class="opacity-5 text-dark" href="javascript:;">Pages</a>
-            </li>
-            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">
-              index
-            </li>
-          </ol>
-          <h6 class="font-weight-bolder mb-0">index</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-            <div class="input-group input-group-outline">
-              <label class="form-label">Type here...</label>
-              <input type="text" class="form-control" />
-            </div>
+
+
+
           </div>
-          <ul class="navbar-nav justify-content-end">
-            <li class="nav-item d-flex align-items-center">
-              <a class="btn btn-outline-primary btn-sm mb-0 me-3" target="_blank"
-                href="https://www.creative-tim.com/builder?ref=navbar-material-dashboard">Online Builder</a>
-            </li>
-            <li class="mt-2">
-              <a class="github-button" href="https://github.com/creativetimofficial/material-dashboard"
-                data-icon="octicon-star" data-size="large" data-show-count="true"
-                aria-label="Star creativetimofficial/material-dashboard on GitHub">Star</a>
-            </li>
+          <ul class="navbar-nav  justify-content-end">
             <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
               <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
                 <div class="sidenav-toggler-inner">
@@ -268,17 +230,16 @@ while ($row = mysqli_fetch_array($result)) {
                 <i class="fa fa-bell cursor-pointer"></i>
               </a>
 
-              <ul class="dropdown-menu dropdown-menu-end px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
+              <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
                 <li class="mb-2">
                   <a class="dropdown-item border-radius-md" href="javascript:;">
                     <div class="d-flex py-1">
                       <div class="my-auto">
-                        <img src="./assets/img/team-2.jpg" class="avatar avatar-sm me-3" />
+                        <img src="./assets/img/team-2.jpg" class="avatar avatar-sm  me-3 ">
                       </div>
                       <div class="d-flex flex-column justify-content-center">
                         <h6 class="text-sm font-weight-normal mb-1">
-                          <span class="font-weight-bold">New message</span>
-                          from Laur
+                          <span class="font-weight-bold">New message</span> from Laur
                         </h6>
                         <p class="text-xs text-secondary mb-0">
                           <i class="fa fa-clock me-1"></i>
@@ -293,12 +254,11 @@ while ($row = mysqli_fetch_array($result)) {
                     <div class="d-flex py-1">
                       <div class="my-auto">
                         <img src="./assets/img/small-logos/logo-spotify.svg"
-                          class="avatar avatar-sm bg-gradient-dark me-3" />
+                          class="avatar avatar-sm bg-gradient-dark  me-3 ">
                       </div>
                       <div class="d-flex flex-column justify-content-center">
                         <h6 class="text-sm font-weight-normal mb-1">
-                          <span class="font-weight-bold">New album</span> by
-                          Travis Scott
+                          <span class="font-weight-bold">New album</span> by Travis Scott
                         </h6>
                         <p class="text-xs text-secondary mb-0">
                           <i class="fa fa-clock me-1"></i>
@@ -311,7 +271,7 @@ while ($row = mysqli_fetch_array($result)) {
                 <li>
                   <a class="dropdown-item border-radius-md" href="javascript:;">
                     <div class="d-flex py-1">
-                      <div class="avatar avatar-sm bg-gradient-secondary me-3 my-auto">
+                      <div class="avatar avatar-sm bg-gradient-secondary  me-3  my-auto">
                         <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1"
                           xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                           <title>credit-card</title>
@@ -346,16 +306,25 @@ while ($row = mysqli_fetch_array($result)) {
               </ul>
             </li>
             <li class="nav-item d-flex align-items-center">
-              <a href="./pages/sign-in.html" class="nav-link text-body font-weight-bold px-0">
+              <a href="./pages/profile.php" class="nav-link text-body font-weight-bold px-0">
                 <i class="fa fa-user me-sm-1"></i>
-
-                <span class="d-sm-inline d-none">Sign In</span>
+                <span class="d-sm-inline d-none">
+                  <?php echo $_SESSION["username"]; ?>
+                </span>
+              </a>
+            </li>
+            <li><span class="d-sm-inline d-none">&nbsp;&nbsp;</span></li>
+            <li class="nav-item d-flex align-items-center">
+              <a href="./pages/logout.php" class="nav-link text-body font-weight-bold px-0">
+                <i class="fa fa-sign-out"></i>
+                <span class="d-sm-inline d-none">Sign out</span>
               </a>
             </li>
           </ul>
         </div>
       </div>
     </nav>
+
     <!-- End Navbar -->
 
     <style>
@@ -400,22 +369,7 @@ while ($row = mysqli_fetch_array($result)) {
     <script src="./gauge/gauge.js?v=1.2"></script>
     <script src="./gauge/gauge_tem.js?v=1.2"></script>
 
-    <script>
-      var win = navigator.platform.indexOf("Win") > -1;
-      if (win && document.querySelector("#sidenav-scrollbar")) {
-        var options = {
-          damping: "0.5",
-        };
-        Scrollbar.init(document.querySelector("#sidenav-scrollbar"), options);
-      }
-    </script>
-
-    <!-- Github buttons -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-
-    <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="./assets/js/material-dashboard.min.js?v=3.1.0"></script>
-
+    
 
   </main>
 
