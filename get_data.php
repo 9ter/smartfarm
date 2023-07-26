d346219f7113c597667e7907815026a149d6bfee
9ter <kasiyapo@gmail.com>
2023-07-26 14:26:48 +0700

    1

--- get_data.php
+++ get_data.php
@@ -7,5 +7,15 @@ $sql = "SELECT * FROM sensor_box_1";
 $result = $conn->query($sql);
 
 
+if ($result->num_rows > 0) {
+    // สร้าง array หรือ object เพื่อเก็บข้อมูลที่ได้จาก MySQL
+    $data = array();
+
+    // วน loop เพื่อดึงข้อมูลแต่ละแถวในตาราง
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
 
 ?>
