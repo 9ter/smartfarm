435ccef97bad7627b55b8910a649e3ca71b16a02
9ter <kasiyapo@gmail.com>
2023-07-26 15:02:49 +0700

    1

--- gauge.js
+++ gauge.js
@@ -1,84 +1,67 @@
-// JS 
-var chart = JSC.chart('chartDiv', { 
-    debug: true, 
-    type: 'gauge', 
-    animation_duration: 1000, 
-    legend_visible: false, 
-    xAxis: { spacingPercentage: 0.25 }, 
-    yAxis: { 
-      defaultTick: { 
-        padding: -5, 
-        label_style_fontSize: '14px'
-      }, 
-      line: { 
-        width: 9, 
-        color: 'smartPalette', 
-        breaks_gap: 0.06 
-      }, 
-      scale_range: [0, 100] 
-    }, 
-    palette: { 
-      pointValue: '{%value/100}', 
-      colors: ['green', 'yellow', 'red'] 
-    }, 
-    defaultTooltip_enabled: false, 
-    defaultSeries: { 
-      angle: { sweep: 180 }, 
-      shape: { 
-        innerSize: '70%', 
-    
-        label: { 
-          text: 
-            '<span color="%color">{%sum:n1}</span><br/><span color="#696969" fontSize="20px">kW</span>', 
-          style_fontSize: '46px', 
-          verticalAlign: 'middle'
-        } 
-      } 
-    }, 
-    series: [ 
-      { 
-        type: 'column roundcaps', 
-        points: [{ id: '1', x: 'speed', y: 0 }] 
-      } 
-    ], 
-    toolbar_items: { 
-      Stop: { 
-        type: 'option', 
-        icon_name: 'system/default/pause', 
-        margin: 10, 
-        boxVisible: true, 
-        label_text: 'Pause', 
-        events: { change: playPause }, 
-        states_select: { 
-          icon_name: 'system/default/play', 
-          label_text: 'Play'
-        } 
-      } 
-    } 
-  }); 
-  var INTERVAL_ID; 
-    
-  //playPause(); 
-    
-  function setGauge(max, y) { 
-    chart 
-      .series(0) 
-      .options({ 
-        points: [{ id: '1', x: 'speed', y: y }] 
-      }); 
-    //chart.annotations('anVal').options({ label_text: JSC.formatNumber(y, 'n1') }); 
-  } 
-    
-  function playPause(val) { 
-    if (val) { 
-      clearInterval(INTERVAL_ID); 
-    } else { 
-      update(); 
-    } 
-  } 
-    
-  function update() { 
-    INTERVAL_ID = setInterval(function() { 
-      setGauge(100, Math.random() * 100); 
-    }, 1200); 
-  } 
+// JS
+var chart = JSC.chart("chartDiv", {
+  debug: true,
+  type: "gauge",
+  animation_duration: 1000,
+  legend_visible: false,
+  xAxis: { spacingPercentage: 0.25 },
+  yAxis: {
+    defaultTick: {
+      padding: -5,
+      label_style_fontSize: "14px",
+    },
+    line: {
+      width: 9,
+      color: "smartPalette",
+      breaks_gap: 0.06,
+    },
+    scale_range: [0, 100],
+  },
+  palette: {
+    pointValue: "{%value/100}",
+    colors: ["green", "yellow", "red"],
+  },
+  defaultTooltip_enabled: false,
+  defaultSeries: {
+    angle: { sweep: 180 },
+    shape: {
+      innerSize: "70%",
+
+      label: {
+        text: '<span color="%color">{%sum:n1}</span><br/><span color="#696969" fontSize="20px">kW</span>',
+        style_fontSize: "46px",
+        verticalAlign: "middle",
+      },
+    },
+  },
+  series: [
+    {
+      type: "column roundcaps",
+      points: [{ id: "1", x: "speed", y: 0 }],
+    },
+  ],
+});
+var INTERVAL_ID;
+
+//playPause();
+
+function setGauge(max, y) {
+  chart.series(0).options({
+    points: [{ id: "1", x: "speed", y: y }],
+  });
+  //chart.annotations('anVal').options({ label_text: JSC.formatNumber(y, 'n1') });
+}
+
+function playPause(val) {
+  if (val) {
+    clearInterval(INTERVAL_ID);
+  } else {
+    update();
+  }
+}
+
+function update() {
+  INTERVAL_ID = setInterval(function () {
+    setGauge(100, Math.random() * 100);
+  }, 1200);
+}
