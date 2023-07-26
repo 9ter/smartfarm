9e25ea4cffd8d5a613a54673166ecd396acd0075
9ter <kasiyapo@gmail.com>
2023-07-26 12:52:00 +0700

    1

--- index.php
+++ index.php
@@ -95,10 +95,10 @@ while ($row = mysqli_fetch_array($result)) {
 
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-     
+      <a class="navbar-brand m-0" href="" target="_blank">
         <img src="./assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
         <span class="ms-1 font-weight-bold text-white">Smartfarm Dashboard </span>
-      
+      </a>
     </div>
 
 
