12398b8fd90e86c76afca0343fb8ad635500a8e8
9ter <kasiyapo@gmail.com>
2023-07-26 16:47:05 +0700

    1

--- gauge/gauge.js
+++ gauge/gauge.js
@@ -1,5 +1,21 @@
 // JS
 
+// ฟังก์ชัน resizeCharts เพิ่มเติมเพื่อปรับขนาดแผนภูมิเมื่อเปลี่ยนขนาดหน้าจอ
+function resizeCharts() {
+  const chartDivs = document.getElementsByClassName("chartDiv");
+  for (let i = 0; i < chartDivs.length; i++) {
+      chartDivs[i].style.maxWidth = "450px";
+      chartDivs[i].style.height = "250px";
+      chartDivs[i].style.margin = "0px auto";
+  }
+}
+
+window.addEventListener("resize", function () {
+  resizeCharts();
+});
+
+resizeCharts();
+
 function setGauge(index, y) {
   switch (index) {
     case 1:
--- gauge/gauge_tem.js
+++ gauge/gauge_tem.js
@@ -1,14 +1,302 @@
-function resizeCharts() {
-    const chartDivs = document.getElementsByClassName("chartDiv");
-    for (let i = 0; i < chartDivs.length; i++) {
-      chartDivs[i].style.maxWidth = "450px";
-      chartDivs[i].style.height = "250px";
-      chartDivs[i].style.margin = "0px auto";
-    }
-  }
-  
-  window.addEventListener("resize", function () {
-    resizeCharts();
-  });
-  
-  resizeCharts();
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var chart1 = JSC.chart("chartDiv1", {
+  debug: true,
+  type: "gauge",
+  animation_duration: 1000,
+  legend_visible: false,
+  xAxis: { spacingPercentage: 0.25 },
+  yAxis: {
+    defaultTick: {
+      padding: -5,
+      label_style_fontSize: "14px",
+    },
+    line: {
+      width: 9,
+      color: "smartPalette",
+      breaks_gap: 0.06,
+    },
+    scale_range: [0, 1000],
+  },
+  palette: {
+    pointValue: "{%value/1000}",
+    colors: ["green", "yellow", "red"],
+  },
+  defaultTooltip_enabled: false,
+  defaultSeries: {
+    angle: { sweep: 180 },
+    shape: {
+      innerSize: "70%",
+
+      label: {
+        text: '<span color="%color">{%sum:n1}</span><br/><span color="#696969" fontSize="20px">kW</span>',
+        style_fontSize: "46px",
+        verticalAlign: "middle",
+      },
+    },
+  },
+  series: [
+    {
+      type: "column roundcaps",
+      points: [{ id: "1", x: "speed", y: 0 }],
+    },
+  ],
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+var chart2 = JSC.chart("chartDiv2", {
+  debug: true,
+  type: "gauge",
+  animation_duration: 1000,
+  legend_visible: false,
+  xAxis: { spacingPercentage: 0.25 },
+  yAxis: {
+    defaultTick: {
+      padding: -5,
+      label_style_fontSize: "14px",
+    },
+    line: {
+      width: 9,
+      color: "smartPalette",
+      breaks_gap: 0.06,
+    },
+    scale_range: [0, 1000],
+  },
+  palette: {
+    pointValue: "{%value/1000}",
+    colors: ["green", "yellow", "red"],
+  },
+  defaultTooltip_enabled: false,
+  defaultSeries: {
+    angle: { sweep: 180 },
+    shape: {
+      innerSize: "70%",
+
+      label: {
+        text: '<span color="%color">{%sum:n1}</span><br/><span color="#696969" fontSize="20px">kW</span>',
+        style_fontSize: "46px",
+        verticalAlign: "middle",
+      },
+    },
+  },
+  series: [
+    {
+      type: "column roundcaps",
+      points: [{ id: "1", x: "speed", y: 0 }],
+    },
+  ],
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+var chart3 = JSC.chart("chartDiv3", {
+  debug: true,
+  type: "gauge",
+  animation_duration: 1000,
+  legend_visible: false,
+  xAxis: { spacingPercentage: 0.25 },
+  yAxis: {
+    defaultTick: {
+      padding: -5,
+      label_style_fontSize: "14px",
+    },
+    line: {
+      width: 9,
+      color: "smartPalette",
+      breaks_gap: 0.06,
+    },
+    scale_range: [0, 1000],
+  },
+  palette: {
+    pointValue: "{%value/1000}",
+    colors: ["green", "yellow", "red"],
+  },
+  defaultTooltip_enabled: false,
+  defaultSeries: {
+    angle: { sweep: 180 },
+    shape: {
+      innerSize: "70%",
+
+      label: {
+        text: '<span color="%color">{%sum:n1}</span><br/><span color="#696969" fontSize="20px">kW</span>',
+        style_fontSize: "46px",
+        verticalAlign: "middle",
+      },
+    },
+  },
+  series: [
+    {
+      type: "column roundcaps",
+      points: [{ id: "1", x: "speed", y: 0 }],
+    },
+  ],
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+var chart4 = JSC.chart("chartDiv4", {
+  debug: true,
+  type: "gauge",
+  animation_duration: 1000,
+  legend_visible: false,
+  xAxis: { spacingPercentage: 0.25 },
+  yAxis: {
+    defaultTick: {
+      padding: -5,
+      label_style_fontSize: "14px",
+    },
+    line: {
+      width: 9,
+      color: "smartPalette",
+      breaks_gap: 0.06,
+    },
+    scale_range: [0, 1000],
+  },
+  palette: {
+    pointValue: "{%value/1000}",
+    colors: ["green", "yellow", "red"],
+  },
+  defaultTooltip_enabled: false,
+  defaultSeries: {
+    angle: { sweep: 180 },
+    shape: {
+      innerSize: "70%",
+
+      label: {
+        text: '<span color="%color">{%sum:n1}</span><br/><span color="#696969" fontSize="20px">kW</span>',
+        style_fontSize: "46px",
+        verticalAlign: "middle",
+      },
+    },
+  },
+  series: [
+    {
+      type: "column roundcaps",
+      points: [{ id: "1", x: "speed", y: 0 }],
+    },
+  ],
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+var chart5 = JSC.chart("chartDiv5", {
+  debug: true,
+  type: "gauge",
+  animation_duration: 1000,
+  legend_visible: false,
+  xAxis: { spacingPercentage: 0.25 },
+  yAxis: {
+    defaultTick: {
+      padding: -5,
+      label_style_fontSize: "14px",
+    },
+    line: {
+      width: 9,
+      color: "smartPalette",
+      breaks_gap: 0.06,
+    },
+    scale_range: [0, 1000],
+  },
+  palette: {
+    pointValue: "{%value/1000}",
+    colors: ["green", "yellow", "red"],
+  },
+  defaultTooltip_enabled: false,
+  defaultSeries: {
+    angle: { sweep: 180 },
+    shape: {
+      innerSize: "70%",
+
+      label: {
+        text: '<span color="%color">{%sum:n1}</span><br/><span color="#696969" fontSize="20px">kW</span>',
+        style_fontSize: "46px",
+        verticalAlign: "middle",
+      },
+    },
+  },
+  series: [
+    {
+      type: "column roundcaps",
+      points: [{ id: "1", x: "speed", y: 0 }],
+    },
+  ],
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+var chart6 = JSC.chart("chartDiv6", {
+  debug: true,
+  type: "gauge",
+  animation_duration: 1000,
+  legend_visible: false,
+  xAxis: { spacingPercentage: 0.25 },
+  yAxis: {
+    defaultTick: {
+      padding: -5,
+      label_style_fontSize: "14px",
+    },
+    line: {
+      width: 9,
+      color: "smartPalette",
+      breaks_gap: 0.06,
+    },
+    scale_range: [0, 1000],
+  },
+  palette: {
+    pointValue: "{%value/1000}",
+    colors: ["green", "yellow", "red"],
+  },
+  defaultTooltip_enabled: false,
+  defaultSeries: {
+    angle: { sweep: 180 },
+    shape: {
+      innerSize: "70%",
+
+      label: {
+        text: '<span color="%color">{%sum:n1}</span><br/><span color="#696969" fontSize="20px">kW</span>',
+        style_fontSize: "46px",
+        verticalAlign: "middle",
+      },
+    },
+  },
+  series: [
+    {
+      type: "column roundcaps",
+      points: [{ id: "1", x: "speed", y: 0 }],
+    },
+  ],
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+var chart7 = JSC.chart("chartDiv7", {
+  debug: true,
+  type: "gauge",
+  animation_duration: 1000,
+  legend_visible: false,
+  xAxis: { spacingPercentage: 0.25 },
+  yAxis: {
+    defaultTick: {
+      padding: -5,
+      label_style_fontSize: "14px",
+    },
+    line: {
+      width: 9,
+      color: "smartPalette",
+      breaks_gap: 0.06,
+    },
+    scale_range: [0, 1000],
+  },
+  palette: {
+    pointValue: "{%value/1000}",
+    colors: ["green", "yellow", "red"],
+  },
+  defaultTooltip_enabled: false,
+  defaultSeries: {
+    angle: { sweep: 180 },
+    shape: {
+      innerSize: "70%",
+
+      label: {
+        text: '<span color="%color">{%sum:n1}</span><br/><span color="#696969" fontSize="20px">kW</span>',
+        style_fontSize: "46px",
+        verticalAlign: "middle",
+      },
+    },
+  },
+  series: [
+    {
+      type: "column roundcaps",
+      points: [{ id: "1", x: "speed", y: 0 }],
+    },
+  ],
+});
