e3b34de444fa2576dc7f35dfddec35f7f02a4182
9ter <kasiyapo@gmail.com>
2023-07-26 13:29:15 +0700

    1

--- index.php
+++ index.php
@@ -335,7 +335,7 @@ while ($row = mysqli_fetch_array($result)) {
     <script src="./assets/js/plugins/perfect-scrollbar.min.js"></script>
     <script src="./assets/js/plugins/smooth-scrollbar.min.js"></script>
 
-    <script src="./gauge.js"></script>
+    
 
 
 </body>
