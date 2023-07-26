86781ad078a1f8a8e5d97520c50b492bfcde3a06
9ter <kasiyapo@gmail.com>
2023-07-26 13:03:56 +0700

    1

--- index.php
+++ index.php
@@ -900,7 +900,59 @@ while ($row = mysqli_fetch_array($result)) {
 
   <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="./assets/js/material-dashboard.min.js?v=3.1.0"></script>
-  <script src="./gauge.js"></script>
+  <script>
+        // data for the gauge chart
+        // you can supply your own values here
+        // max is the Gauge's maximum value
+        var data = {
+            value: 200,
+            max: 300,
+            label: "Progress"
+        };
+
+        // Chart.js chart's configuration
+        // We are using a Doughnut type chart to 
+        // get a Gauge format chart 
+        // This is approach is fine and actually flexible
+        // to get beautiful Gauge charts out of it
+        var config = {
+            type: 'doughnut',
+            data: {
+                labels: [data.label],
+                datasets: [{
+                    data: [data.value, data.max - data.value],
+                    backgroundColor: ['rgba(54, 162, 235, 0.8)', 'rgba(0, 0, 0, 0.1)'],
+                    borderWidth: 0
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                cutoutPercentage: 85,
+                rotation: -90,
+                circumference: 180,
+                tooltips: {
+                    enabled: false
+                },
+                legend: {
+                    display: false
+                },
+                animation: {
+                    animateRotate: true,
+                    animateScale: false
+                },
+                title: {
+                    display: true,
+                    text: data.label,
+                    fontSize: 16
+                }
+            }
+        };
+
+        // Create the chart
+        var chartCtx = document.getElementById('gaugeChart').getContext('2d');
+        var gaugeChart = new Chart(chartCtx, config);
+    </script>
 </body>
 
 </html>
