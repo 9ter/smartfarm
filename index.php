b289f6df7ee19fdda099f7a4d289d85a475465a3
9ter <kasiyapo@gmail.com>
2023-07-26 17:06:08 +0700

    1

--- index.php
+++ index.php
@@ -369,7 +369,23 @@ while ($row = mysqli_fetch_array($result)) {
     <script src="./gauge/gauge.js?v=1.2"></script>
     <script src="./gauge/gauge_tem.js?v=1.2"></script>
 
-    
+
+
+    <script>
+      var win = navigator.platform.indexOf("Win") > -1;
+      if (win && document.querySelector("#sidenav-scrollbar")) {
+        var options = {
+          damping: "0.5",
+        };
+        Scrollbar.init(document.querySelector("#sidenav-scrollbar"), options);
+      }
+    </script>
+
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+    <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="./assets/js/material-dashboard.min.js?v=3.1.0"></script>
 
   </main>
 
