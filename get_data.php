eb1e5998a8032471f6775442e78503a321068204
9ter <kasiyapo@gmail.com>
2023-07-26 14:24:47 +0700

    1

--- get_data.php
+++ get_data.php
@@ -2,9 +2,7 @@
 
 include 'config.php';
 
-// คำสั่ง SQL เพื่อดึงข้อมูลที่ต้องการจากฐานข้อมูล
-$sql = "SELECT value FROM sensor_box_1 ORDER BY id DESC LIMIT 1";
-$result = $conn->query($sql);
+
 
 
 
